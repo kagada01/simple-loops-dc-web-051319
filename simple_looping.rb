5f731b4a9e820b6587fdb30c35ab4480ce29ea1b
Dan Kagan <dankagan7@gmail.com>
2019-03-27 22:49:09 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -40,7 +40,7 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-    for time in (0..number_of_times) do
+    for time in (1..number_of_times) do
       puts phrase
       
     end
